a63d70a504d1f89b9db8a3b139044e3965ef70a9
Ezeial <eziogiraldi.dev@gmail.com>
2021-11-25 06:22:16 +0100

    up sandbox

--- sandbox/main.c
+++ sandbox/main.c
@@ -3,7 +3,7 @@
 #include <sys/time.h>
 #include <stdlib.h>
 
-#define N 6
+#define N 5
 #define FREE 0
 #define TAKEN 1
 #define LEFTFORK(n) n
@@ -23,10 +23,13 @@ void	*philosopher(int n)
 	size_t		i;
 	t_timeval	tv;
 	t_timezone	tz;
-	long		last_time;
+
+	long		last_usecond;
+	long		last_second;
 
 	gettimeofday(&tv, &tz);
-	last_time = tv.tv_usec;
+	last_usecond = tv.tv_usec;
+	last_second = tv.tv_sec;
 	while (1)
 	{
 		waiting = 1;
@@ -34,13 +37,13 @@ void	*philosopher(int n)
 		while (waiting)
 		{
 			gettimeofday(&tv, &tz);
-			if (tv.tv_usec - last_time > 2 * 1000000)
+			if (tv.tv_sec - last_second >= 4 && tv.tv_usec - (10000) > last_usecond)
 			{
-				printf("[%d] DIE AFTER [%llu]\n", n, tv.tv_usec - last_time);
-				return (NULL);
+				printf("[%d] DIE AFTER [%lds%ldu]\n", n, tv.tv_sec - last_second, tv.tv_usec - last_usecond);
+				exit(0);
 			}
-			else
-				printf("last time %ld tv_usec %ld difference %ld\n", last_time, tv.tv_usec, tv.tv_usec - last_time);
+			// else
+				// printf("last time %ld tv_usec %ld difference %ldu%ld\n", last_second, tv.tv_sec, tv.tv_sec - last_second, tv.tv_usec - last_usecond);
 			pthread_mutex_lock(&mutex);
 			if (ISLEFT_FREE(n) && ISRIGHT_FREE(n))
 			{
@@ -52,16 +55,17 @@ void	*philosopher(int n)
 			pthread_mutex_unlock(&mutex);
 		}
 		printf("\033[0;36m[%d] EATING\n\033[0m", n);
-		sleep(2);
+		usleep(2000000);
 		pthread_mutex_lock(&mutex);
 		printf("[%d] REALEASE FORKS %d and %d\n",n,  LEFTFORK(n), RIGHTFORK(n));
 		forks[LEFTFORK(n)] = FREE;
 		forks[RIGHTFORK(n)] = FREE;
 		pthread_mutex_unlock(&mutex);
 		printf("[%d] SLEEPING\n", n);
-		sleep(2);
+		usleep(2000000);
 		gettimeofday(&tv, &tz);
-		last_time = tv.tv_usec;
+		last_second= tv.tv_sec;
+		last_usecond = tv.tv_usec;
 	}
 	return (NULL);
 }
